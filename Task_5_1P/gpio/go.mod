fe2301c8c620b357823117c8ae93965e8910eee9
Peter Stacey <pstacey@deakin.edu.au>
2021-03-08 19:30:07 +1100

    added task 5.1 and 6.1

---
+++ Task_5_1P/gpio/go.mod
@@ -0,0 +1,5 @@
+module github.com/pscompsci/Task_5_1P/gpio
+
+go 1.13
+
+require github.com/stianeikeland/go-rpio/v4 v4.4.0
---
+++ Task_5_1P/gpio/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+var (
+	pin = rpio.Pin(10)
+)
+
+func main() {
+	setupCloseHandler()
+
+	if err := rpio.Open(); err != nil {
+		fmt.Printf("error opening the rpio connection: %v\n", err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	pin.Output()
+
+	for {
+		pin.Toggle()
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func setupCloseHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\n- Ctrl+C pressed in Terminal")
+		os.Exit(0)
+	}()
+}
