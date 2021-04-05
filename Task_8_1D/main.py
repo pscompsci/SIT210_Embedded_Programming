dad37d5bcdcb53c472eab5f42dab9aa7f9d2ddad
Peter Stacey <pstacey@deakin.edu.au>
2021-04-05 20:13:34 +1000

    Added 8.1D code

---
+++ Task_8_1D/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+''' Task 8.1D - Raspberry Pi I2C
+
+    Student ID:     219011171
+    Student Name:   Peter Stacey
+'''
+
+from signal import signal, SIGTERM, SIGHUP, pause
+from time import sleep
+from threading import Thread
+from gpiozero import DistanceSensor
+from rpi_lcd import LCD
+
+
+running = True
+message = ""
+
+lcd = LCD()
+sensor = DistanceSensor(echo=20, trigger=21)
+
+def safe_exit():
+    exit(1)
+
+def read_distance():
+    global message
+    while running:
+        message = f'Distance: {sensor.value:1.2f} m'
+        print(message)
+        sleep(0.1)
+
+def update_display():
+    global message
+    while running:
+        lcd.text(message, 1)
+        sleep(0.25)
+
+def main():
+    try:
+        signal(SIGTERM, safe_exit)
+        signal(SIGHUP, safe_exit)
+        reader = Thread(target=read_distance, daemon=True)
+        display = Thread(target=update_display, daemon=True)
+        reader.start()
+        display.start()
+        pause()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        global running
+        running = False
+        reader.join()
+        display.join()
+        lcd.clear()
+        sensor.close()
+
+if __name__ == '__main__':
+    main()
